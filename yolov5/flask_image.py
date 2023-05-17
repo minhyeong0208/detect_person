2d1198d85fbe6dc0a98dfc16121389442cac9172
Minhyeong Lee <minhyoungi0208@gamil.com>
2023-05-17 14:33:38 +0900

    이미지의 경우 detect 성공

--- yolov5/flask_image.py
+++ yolov5/flask_image.py
@@ -1,13 +1,15 @@
 from flask import Flask, jsonify
 from detect import run
 
+#cnt, lab = run(weights='./runs/train/person_yolov5s_results/weights/best.pt', conf_thres=0.1, source='../carrier.jpg', exist_ok=True, line_thickness=2)
+#print(cnt)
+#print(lab)
+
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def object_count():
-    # cap = cv2.VideoCapture("../test.mp4") 
-    
-    cnt, lab = run(weights='./runs/train/person_yolov5s_results/weights/best.pt', conf_thres=0.5, source='../carrier.jpg', exist_ok=True, line_thickness=2)
+    cnt, lab = run(weights='./runs/train/person_yolov5s_results/weights/best.pt',conf_thres=0.5, source='../carrier.jpg', exist_ok=True, line_thickness=2)
     length = len(cnt)
     dict = {}
     for i in range(length):
